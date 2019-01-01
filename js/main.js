26161f7d31f56276a4fd08232d8180aa3f873190
Frank Eduardo <feac80@hotmail.com>
2019-01-01 15:45:00 +0100

    fix align issue

--- js/main.js
+++ js/main.js
@@ -1,123 +1,116 @@
-(function() {
-  'use strict';
-  var API_KEY = 'AIzaSyAxOgfzAeZGuz0fAoq9yUo4PP-LcVUjBKs';
-  var cx = '008346752897722787957:ujvph0wixoi';
+// (function() {
+'use strict';
+var API_KEY = 'AIzaSyBuPZIwjqgs4a0oBqyg_ZzAZAkqxKrGpt4';
+var cx = '006753618627947867668:4fb2xk26yb0';
 
-  var form = document.querySelector('#search');
+// var respond = {
+//   error: {}
+// };
+var form = document.querySelector('#search');
 
-  //in case the image is not loaded
+//in case the image is not loaded
 
-  function imgError(image) {
+function imgError(image) {
+  try {
+    image.onerror = '';
+    // console.log('Image error:' + image.src);
+    image.src = '../img/no-image-available.jpg';
+    throw new Error('Image not available');
+  } catch (e) {
+    // console.log(e);
+  }
+  return true;
+}
+
+//This function displays the search result in their respective containers
+function updatePage(newData) {
+  document.getElementById('results').innerHTML = '';
+  document.getElementById('images').innerHTML = '';
+
+  var response = JSON.parse(newData);
+  // console.log(response);
+  for (var i = 0; i < response.items.length; i++) {
+    // in production code, item.htmlTitle should have the HTML entities escaped.'&q=' + encodeURIComponent(searchTerm);
+    var item = response.items[i];
+    if (item.pagemap != undefined && item.pagemap.cse_image != undefined) {
+      var src = item.pagemap.cse_image[0].src;
+      // var url = (response.items[i].
+      //make visible the results ul box
+      document.getElementById('results').style.visibility = 'visible';
+      document.getElementById('results').innerHTML +=
+        '<li> <a href=' +
+        item.link +
+        ' target=' +
+        "'_blank'" +
+        '>' +
+        item.htmlTitle +
+        ' </a></li>';
+      document.getElementById('pages').style.visibility = 'visible';
+      document.getElementById('images').innerHTML +=
+        '<a href=' +
+        item.link +
+        ' target=' +
+        "'_blank'" +
+        '><img ' +
+        "class='images'" +
+        "onerror='imgError(this)'" +
+        'src=' +
+        src +
+        ' /></a>';
+    }
+  }
+}
+function buildUrl(startIndex) {
+  var startValue = startIndex + (startIndex - 1) * 9;
+
+  var searchTerm = document.getElementById('search-item').value;
+  var searchQuery = searchTerm.trim();
+  var url =
+    'https://www.googleapis.com/customsearch/v1?key=' +
+    API_KEY +
+    '&q=' +
+    searchQuery +
+    '&cx=' +
+    cx +
+    '&num=10' +
+    '&start=' +
+    startValue;
+  return url;
+}
+function sendRequest(startIndex) {
+  var Http = new XMLHttpRequest();
+  var url = buildUrl(startIndex);
+  Http.open('GET', url, true);
+
+  Http.send();
+  Http.onreadystatechange = function(e) {
+    if (Http.readyState == 4 && Http.status == 200) {
+      updatePage(Http.responseText);
+    }
     try {
-      image.onerror = '';
-      // console.log('Image error:' + image.src);
-      image.src = '../img/no-image-available.jpg';
-      throw new Error('Image not available');
+      if (Http.readyState == 4 && Http.status !== 200) {
+        console.log(Http.status);
+
+        throw new Error(Http.status);
+      }
     } catch (e) {
-      // console.log(e);
+      console.log(e);
+      // window.location.href = 'http://127.0.0.1:5500/error.html';
     }
-    return true;
-  }
-
-  //This function displays the search result in their respective containers
-  function updatePage(newData) {
-    document.getElementById('results').innerHTML = '';
-    document.getElementById('images').innerHTML = '';
-
-    var response = JSON.parse(newData);
-
-    for (var i = 0; i < response.items.length; i++) {
-      // in production code, item.htmlTitle should have the HTML entities escaped.'&q=' + encodeURIComponent(searchTerm);
-      var item = response.items[i];
-      if (item.pagemap != undefined && item.pagemap.cse_image != undefined) {
-        var src = item.pagemap.cse_image[0].src;
-        // var url = (response.items[i].
-        document.getElementById('results').innerHTML +=
-          '<li> <a href=' +
-          item.link +
-          ' target=' +
-          "'_blank'" +
-          '>' +
-          item.htmlTitle +
-          ' </a></li>';
-
-        document.getElementById('images').innerHTML +=
-          '<a href=' +
-          item.link +
-          ' target=' +
-          "'_blank'" +
-          '><img ' +
-          "class='images'" +
-          "onerror='imgError(this)'" +
-          'src=' +
-          src +
-          ' /></a>';
-      }
-    }
-  }
-  function buildUrl(startIndex) {
-    var startValue = startIndex + (startIndex - 1) * 9;
-
-    var searchTerm = document.getElementById('search-item').value;
-    var searchQuery = searchTerm.trim();
-    var url =
-      'https://www.googleapis.com/customsearch/v1?key=' +
-      API_KEY +
-      '&q=' +
-      searchQuery +
-      '&cx=' +
-      cx +
-      '&num=10' +
-      '&start=' +
-      startValue;
-    return url;
-  }
-  function sendRequest(startIndex) {
-    var Http = new XMLHttpRequest();
-    var url = buildUrl(startIndex);
-    Http.open('GET', url, true);
-
-    Http.send();
-    Http.onreadystatechange = function(e) {
-      if (Http.readyState == 4 && Http.status == 200) {
-        updatePage(Http.responseText);
-      }
-      try {
-        if (Http.readyState == 4 && Http.status !== 200) {
-          console.log(Http.status);
-
-          throw new Error(Http.status);
-        }
-      } catch (e) {
-        console.log(e);
-        window.location.href =
-          'https://google-search-engine-api.herokuapp.com/error.html';
-      }
-    };
-  }
-
-  form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    var currentPage = 1;
-    sendRequest(currentPage);
-    document.getElementById('pages').style.visibility = 'visible';
-  });
-  document.getElementById('pages').onclick = function(e) {
-    var activePage = document.getElementsByClassName('active');
-    activePage[0].classList.toggle('active');
-    // console.log(element);
-    e.srcElement.classList.toggle('active');
-    sendRequest(Number(e.srcElement.textContent));
   };
-})();
-// function imgError(image) {
-//   try {
-//     image.onerror = '';
-//     // console.log('Image error:' + image.src);
-//     image.src = '../img/no-image-available.jpg';
-//     throw new Error('Image not available');
-//   } catch (e) {
-//     // console.log(e);
-//   }
-//   return true;
-// }
+}
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  var currentPage = 1;
+  sendRequest(currentPage);
+  // .search-results
+});
+document.getElementById('pages').onclick = function(e) {
+  var activePage = document.getElementsByClassName('active');
+  activePage[0].classList.toggle('active');
+  // console.log(element);
+  e.srcElement.classList.toggle('active');
+  sendRequest(Number(e.srcElement.textContent));
+};
+// })();
